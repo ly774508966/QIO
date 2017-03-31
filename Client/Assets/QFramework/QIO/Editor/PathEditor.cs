05f816a4a1a2b499f7b3f07e7f104073178e4cf8
liangxiegame <liangxiegame@163.com>
2017-03-31 23:39:36 +0800

    0.0.1.0 初始化QIO

---
+++ Client/Assets/QFramework/QIO/Editor/PathEditor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+
+namespace QFramework.Editor
+{
+    public class AppConfigEditor
+    {
+        [MenuItem("Assets/AppConfig/Build AppConfig")]
+        public static void BuildAppConfig()
+        {
+
+			PathConfig data = null;
+            string folderPath = EditorUtils.GetSelectedDirAssetsPath();
+            string spriteDataPath = folderPath + "Path.asset";
+
+			data = AssetDatabase.LoadAssetAtPath<PathConfig>(spriteDataPath);
+            if (data == null)
+            {
+				data = ScriptableObject.CreateInstance<PathConfig>();
+                AssetDatabase.CreateAsset(data, spriteDataPath);
+            }
+
+            EditorUtility.SetDirty(data);
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
---
+++ Client/Assets/QFramework/QIO/PathConfig.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace QFramework {
+
+	public enum PATH_ROOT {
+		ApplicationPersistentDataPath,
+		ApplicationDataPath,
+		ApplicationStreamingAssetsPath
+	}
+
+	[System.Serializable]
+	public class PathItem {
+		[Header("描述")]
+		[SerializeField] string m_Description;
+		[SerializeField] PATH_ROOT m_Root;
+		[SerializeField] string m_Name;
+		[SerializeField] string m_Path;
+
+		public string Path {
+			get {
+				switch (m_Root) {
+				case PATH_ROOT.ApplicationDataPath:
+					return Application.dataPath + "/" + m_Path;
+				case PATH_ROOT.ApplicationPersistentDataPath:
+					return PATH_ROOT.ApplicationPersistentDataPath + "/" + m_Path;
+				case PATH_ROOT.ApplicationStreamingAssetsPath:
+					return PATH_ROOT.ApplicationStreamingAssetsPath + "/" + m_Path;
+				}
+				return m_Path;
+			}
+		}
+
+	}
+		
+	/// <summary>
+	/// Path配置
+	/// </summary>
+	public class PathConfig : ScriptableObject {
+		[SerializeField]  string Description;
+		[SerializeField]  List<PathItem> m_PathList;
+
+		public List<PathItem> List {
+			get {
+				return m_PathList;
+			}
+		}
+	}
+}
